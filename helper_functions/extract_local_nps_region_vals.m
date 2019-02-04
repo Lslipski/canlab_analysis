a4463e53f949a5874e52f19ea935911f71234182
Luke Slipski <lslipski602@gmail.com>
2019-02-04 14:37:06 -0700

    includes basic input checks and outputs data object with only data for nps region of interest

--- helper_functions/extract_local_nps_region_vals.m
+++ helper_functions/extract_local_nps_region_vals.m
@@ -30,7 +30,7 @@ function NPS_region_vals = extract_local_nps_region_vals(fmri_data_object, my_re
 %   2.) An NPS region of interest: This can be any of the positive or
 %   negative regions of the NPS, which include:
 %
-%       posnames: {'vermis'  'rIns'  'rV1'  'rThal'  'lIns'  'rdpIns'  'rS2_Op'  'dACC'}
+%       posnames: {'vermis'  'rIns'  'rV1'  'rThal'  'lIns'  'rdpInsDATA_STRUCT'  'rS2_Op'  'dACC'}
 %       negnames: {'rLOC'  'lLOC'  'rpLOC'  'pgACC'  'lSTS'  'rIPL'  'PCC'}
 %
 %  :Outputs:
@@ -56,63 +56,66 @@ function NPS_region_vals = extract_local_nps_region_vals(fmri_data_object, my_re
 %        by conditions
 %        G.) contrast_colors: Colors pulled from input fmri_data_object for
 %        plotting by contrasts
-%
+%:Examples:
+% NPS_region_vals = extract_local_nps_region_vals(DAT, 'dACC')
 
 
-% INPUTS
+% Get INPUTS
+region = my_region;
+in_DAT = fmri_data_object;
 
-myregion = 'dACC';
-%myregion = 'rdpIns';
+% check input region and designate pos or neg NPS 
+if sum(strcmp(in_DAT.NPSsubregions.posnames, region)) == 1
+    pos_or_neg_field = 'npspos_by_region';  % set flag field to positive
+    wh_region = strcmp(in_DAT.NPSsubregions.posnames, region);  % find index of input region
+    fprintf('Input region is an NPS Pos region.')
+    
+elseif sum(strcmp(in_DAT.NPSsubregions.negnames, region)) == 1
+    pos_or_neg_field = 'npsneg_by_region';  % set flag field to negative
+    wh_region = strcmp(in_DAT.NPSsubregions.negnames, region);  % find index of input region
+    fprintf('Input region is an NPS Neg region.')
+    
+else
+    error('Bad Region Name')
+end
 
-% DAT
+% Check input data for specified region for each condition
+region_index = find(wh_region);
+for i = 1:length(in_DAT.NPSsubregions.(pos_or_neg_field))
+    if size(in_DAT.NPSsubregions.npspos_by_region{i}, 2) < region_index
+        warning('Input object does not have NPS Data for specified region for every condition')
+    end
 
-% FUNCTION - GET DATA
-% see documentation_template.m
 
-DATA_STRUCT = [];
-DATA_STRUCT.region = myregion;
+NPS_region_vals = [];
+NPS_region_vals.region = region;
 
-k = length(DAT.conditions);
-kc = length(DAT.contrastnames);
-
-% find index of region
-wh_region = strcmp(DAT.NPSsubregions.posnames, myregion);
-
-pos_or_neg_field = 'npspos_by_region';
+k = length(in_DAT.conditions);
+kc = length(in_DAT.contrastnames);
 
 % check for problems
 if sum(wh_region) > 1 || sum(wh_region) == 0, error('Bad region name'); end
 
 % get the condition data
-DATA_STRUCT.condition_data = cell(1, k);
-DATA_STRUCT.conditions = DAT.conditions;
+NPS_region_vals.condition_data = cell(1, k);
+NPS_region_vals.conditions = in_DAT.conditions;
 
 for i = 1:k
-    DATA_STRUCT.condition_data{i} = DAT.NPSsubregions.(pos_or_neg_field){i}(:, wh_region);
+    NPS_region_vals.condition_data{i} = in_DAT.NPSsubregions.(pos_or_neg_field){i}(:, wh_region);
 end
 
 % get the contrast data
-DATA_STRUCT.contrastnames = DAT.contrastnames;
+NPS_region_vals.contrastnames = in_DAT.contrastnames;
 
 myfield = [pos_or_neg_field '_contrasts'];
 
 for i = 1:kc
-    DATA_STRUCT.contrast_data{i} = DAT.NPSsubregions.(myfield){i}(:, wh_region);
+    NPS_region_vals.contrast_data{i} = in_DAT.NPSsubregions.(myfield){i}(:, wh_region);
 end
 
 % Copy colors for use later in plotting
 
-DATA_STRUCT.colors = DAT.colors;
-DATA_STRUCT.contrastcolors = DAT.contrastcolors;
+NPS_region_vals.colors = in_DAT.colors;
+NPS_region_vals.contrastcolors = in_DAT.contrastcolors;
 
-
-
-
-
-
-
-
-
-
-
-
+end
